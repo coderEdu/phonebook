f09b0bba2f1cdecfcdd904198fc8f070e639214a
coderEdu <educoder78@gmail.com>
2022-07-27 21:23:04 -0300

    a litle change

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <?php session_start(); ?>
 <!doctype html>
 <html lang="en">
-  <head>
+  <head> 
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
